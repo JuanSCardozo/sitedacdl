a3cf017ef5d74cb458ea3aed03ccf64fb8b3d895
JuanSCardozo <153205286+JuanSCardozo@users.noreply.github.com>
2024-01-22 15:41:15 -0300

    volta do header

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')){
+    if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "imagens/menu_white_36dp.svg";
+        document.querySelector('.icon').src ="imagens/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "imagens/close_white_36dp.svg";
+        document.querySelector('.icon').src ="imagens/close_white_36dp.svg";
     }
+}
 
 let count = 1;
 document.getElementById("radio1").checked = true;
